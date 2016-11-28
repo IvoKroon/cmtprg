ee3a6cffa7eb139a6b85f578e8406f1a17a68680
Ivo Kroon <ivokroon@192.168.2.240>
2016-11-28 16:02:56 +0100

    Update first rest api

--- app.js
+++ app.js
@@ -1,43 +1,40 @@
 /**
  * Created by ivokroon on 17/11/2016.
  */
-var express = require("express");
-    // mongoose = require("mongoose");
+var express = require("express"),
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser");
 
-// var db = mongoose.connect("mongodb://localhost/bookApi");
+//makes connection if there is no connection it makes the connection.
+var db = mongoose.connect("mongodb://localhost/bookApi");
 
-// var Book = require('./models/bookModel');
+var Book = require('./models/bookModel');
 
 
 var app = express();
 
 var port = process.env.PORT || 3000;
 
-var bookRouter = express.Router();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
-// bookRouter.route('/Books')
-//     .get(function (req,res) {
-//        Book.find(function (err, books) {
-//            if(err)
-//                console.log(err);
-//            else
-//                res.json(books);
-//        });
-//        // res.json(res);
-// });
+bookRouter = require('./routes/bookRoutes')(Book);
 
+
+// var error = {error: "error"};
 // bookRouter.route('/Books/add')
-//     .get(function(req,res){
-//
+//     .get(function (req, res) {
+//         res.json(error);
+//         // db.bookModel.insert( { title: "card", author: "test" } )
 //     });
 
 
-// app.use('/api', bookRouter);
+app.use('/api/books', bookRouter);
 
-app.get('/', function(req, res){
-   res.send('Welcome to my app bla!');
+app.get('/', function (req, res) {
+    res.json({testing:"api"});
 });
 
 app.listen(port, function () {
-   console.log("Running on PORT " + port);
+    console.log("Running on PORT " + port);
 });
