1ea69d9b13758f307eab8801d50dcf702bc0cbbd
Ivo Kroon <ivokroon@192.168.2.58>
2016-12-04 16:17:43 +0100

    links update

--- controller/bookController.js
+++ controller/bookController.js
@@ -36,6 +36,7 @@ var bookController = function(Book){
                 var items = new Object();
                 items.items = returnBooks;
                 items._links = {};
+                items._links.self = {}
                 items._links.self.href = 'http://' + req.headers.host + '/api/books/';
                 items.pagination = {"currentPage":"1"};
                 // <currentPage>1</currentPage>
