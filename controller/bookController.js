9e9e1f86bee192eefb3cef2504424a5a5ce09790
Ivo Kroon <ivokroon@192.168.2.86>
2016-12-12 21:12:21 +0100

    getting crazy of this

--- controller/bookController.js
+++ controller/bookController.js
@@ -15,6 +15,7 @@ var bookController = function (Book) {
 
     var get = function (req, res) {
         var query = {};
+        // console.log(count);
         if (req.accepts('json')) {
             if (req.query.genre) {
                 query.genre = req.query.genre;
@@ -24,12 +25,18 @@ var bookController = function (Book) {
                 // query.limit(2);
                 // console.log(req.query.start);
                 var limit = parseInt(req.query.limit);
-                console.log(limit);
+                // console.log(limit);
 
                 Book.find(query).limit(parseInt(limit)).skip(1).exec(function (err, books){console.log(books)});
             }
-            Book.find().limit(2);
-            Book.find(query, function (err, books) {
+            var startQuery = Book.find(query);
+            if(req.query.start && req.query.limit){
+                startQuery.limit(parseInt(req.query.limit));
+                startQuery.skip(parseInt(req.query.start) - 1);
+            }
+
+           startQuery.exec(function (err, books) {
+               // console.log(books);
                 if (err)
                     res.status(500).send(err);
                 else
@@ -44,28 +51,41 @@ var bookController = function (Book) {
                     // newBook.pagination = {};
                     returnBooks.push(newBook);
                 });
-                var items = new Object();
+                Book.find().count(function (err, count) {
+                    console.log(count);
+
+                // console.log(amount);
+                var items = {};
                 var selfLink = 'http://' + req.headers.host + '/api/books/';
+                var startLink = selfLink;
+                var lastLink = selfLink;
+                if(req.query.start && req.query.limit){
+                    startLink = 'http://' + req.headers.host + '/api/books/?start=' + req.query.start + '&limit=' + req.query.limit;
+                    lastLink = 'http://' + req.headers.host + '/api/books/?start=' + req.query.start + '&limit=' + req.query.limit;
+                }
                 items.items = returnBooks;
                 items._links = {};
                 items._links.self = {};
                 items._links.self.href = selfLink;
 
+                var totalPages = Math.ceil(count / parseInt(req.query.limit));
+
                 items.pagination = {
-                    currentPage:1,
-                    currentItems:returnBooks.length,
-                    totalPages:1,
-                    totalItems:returnBooks.length
+                    currentPage:req.query.start,
+                    currentItems:req.query.limit,
+                    totalPages:totalPages.toString(),
+                    totalItems:count.toString()
                 };
 
                 items.pagination._links = {};
-                items.pagination._links.first = {page:1, href:selfLink};
-                items.pagination._links.last = {page:1, href:selfLink};
+                items.pagination._links.first = {page:1, href:startLink};
+                items.pagination._links.last = {page:totalPages.toString(), href:selfLink};
                 items.pagination._links.previous = {page:1, href:selfLink};
                 items.pagination._links.next = {page:1, href:selfLink};
 
                 res.json(items);
             });
+           });
         } else {
             var err = "Filetype not accepted";
             res.status(400).send(err);
