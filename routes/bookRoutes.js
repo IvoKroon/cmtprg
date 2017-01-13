9c5007f87c1eb130a387f00d29116dfc40b21fcf
Ivo Kroon <ivokroon@mbp-van-ivo.home>
2017-01-13 14:38:00 +0100

    updated allow header

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -14,6 +14,7 @@ var routes = function (Book) {
             option.Get = "Get all the books";
             option.Post = "Make a new book";
             res.header('Access-Control-Allow-Methods','POST, GET, OPTIONS');
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
             // res.header('Allow-header','application/json');
             res.json(option);
